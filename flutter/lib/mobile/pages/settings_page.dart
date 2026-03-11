83fc18fe5297dc73ace10027315cd3ae794bc85d
jiaying <1076697947@qq.com>
2026-03-11 09:10:25 +0800

    调整警告框5s后可点击，修复了函数调用不当的错误

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -996,19 +996,24 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
             )
           ],
         ),
-        CustomSection(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 6, bottom: 18),
-            child: const Center(
-              child: Text(
-                '基于开源软件构建 · 详见用户协议',
-                style: TextStyle(
-                  fontSize: 10,
-                  color: Colors.grey,
+        SettingsSection(
+          title: const Text(''),
+          tiles: const [
+            SettingsTile(
+              title: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 6, bottom: 18),
+                  child: Text(
+                    '基于开源软件构建 · 详见用户协议',
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.grey,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ],
     );
