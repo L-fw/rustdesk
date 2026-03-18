ba70fc41dc01f2f4040f65a7de741b91f46bb424
jiaying <1076697947@qq.com>
2026-03-18 16:13:48 +0800

    回退

--- flutter/android/app/src/main/kotlin/com/gamwing/cloudlink/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/gamwing/cloudlink/MainService.kt
@@ -241,9 +241,8 @@ class MainService : Service() {
         initNotification()
 
         // keep the config dir same with flutter
-        // val prefs = applicationContext.getSharedPreferences(KEY_SHARED_PREFERENCES, FlutterActivity.MODE_PRIVATE)
-        // val configPath = prefs.getString(KEY_APP_DIR_CONFIG_PATH, "") ?: ""
-        val configPath = "${applicationContext.filesDir.absolutePath}/RustDesk"
+        val prefs = applicationContext.getSharedPreferences(KEY_SHARED_PREFERENCES, FlutterActivity.MODE_PRIVATE)
+        val configPath = prefs.getString(KEY_APP_DIR_CONFIG_PATH, "") ?: ""
         FFI.startServer(configPath, "")
 
         createForegroundNotification()
