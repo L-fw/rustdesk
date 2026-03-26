8577912183691992c7f52fbef5a9416b776ea6e5
jiaying <1076697947@qq.com>
2026-03-26 10:57:48 +0800

    加入实时环境查看的功能

--- flutter/lib/mobile/pages/scan_page.dart
+++ flutter/lib/mobile/pages/scan_page.dart
@@ -1,7 +1,9 @@
-import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+import '../../common.dart';
 
 class ScanPage extends StatefulWidget {
   @override
@@ -9,93 +11,66 @@ class ScanPage extends StatefulWidget {
 }
 
 class _ScanPageState extends State<ScanPage> {
-  File? _capturedImage;
-  final ImagePicker _picker = ImagePicker();
-  bool _useRearCamera = true;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (isAndroid && controller != null) {
+      controller!.pauseCamera();
+    } else if (controller != null) {
+      controller!.resumeCamera();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('拍照'),
+        title: const Text('摄像头'),
         actions: [
-          IconButton(
-            icon: Icon(Icons.switch_camera),
-            iconSize: 32.0,
-            color: Colors.white,
-            onPressed: () {
-              setState(() {
-                _useRearCamera = !_useRearCamera;
-              });
-            },
-          ),
+          _buildFlashToggleButton(),
         ],
       ),
-      body: _capturedImage != null ? _buildPreview() : _buildPlaceholder(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _takePhoto,
-        child: Icon(Icons.camera_alt),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: _buildCameraView(context),
     );
   }
 
-  Widget _buildPlaceholder() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.camera_alt, size: 80, color: Colors.grey[400]),
-          SizedBox(height: 16),
-          Text(
-            '点击下方按钮拍照',
-            style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-          ),
-          SizedBox(height: 8),
-          Text(
-            _useRearCamera ? '当前: 后置摄像头' : '当前: 前置摄像头',
-            style: TextStyle(fontSize: 14, color: Colors.grey[500]),
-          ),
-        ],
-      ),
+  Widget _buildCameraView(BuildContext context) {
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onViewCreated,
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
     );
   }
 
-  Widget _buildPreview() {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        Image.file(
-          _capturedImage!,
-          fit: BoxFit.contain,
-        ),
-        Positioned(
-          top: 16,
-          right: 16,
-          child: FloatingActionButton.small(
-            heroTag: 'close',
-            onPressed: () {
-              setState(() {
-                _capturedImage = null;
-              });
-            },
-            child: Icon(Icons.close),
-          ),
-        ),
-      ],
-    );
+  void _onViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
   }
 
-  Future<void> _takePhoto() async {
-    final XFile? photo = await _picker.pickImage(
-      source: ImageSource.camera,
-      preferredCameraDevice:
-          _useRearCamera ? CameraDevice.rear : CameraDevice.front,
-    );
-    if (photo != null) {
-      setState(() {
-        _capturedImage = File(photo.path);
-      });
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    if (!p) {
+      showToast('No permission');
     }
+  }
+
+  Widget _buildFlashToggleButton() {
+    return IconButton(
+      color: Colors.yellow,
+      icon: Icon(Icons.flash_on),
+      iconSize: 32.0,
+      onPressed: () async {
+        await controller?.toggleFlash();
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
