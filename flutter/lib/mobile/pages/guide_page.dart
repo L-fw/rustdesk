ddec35dc31710460dcfe910e9c4645f0baac89eb
jiaying <1076697947@qq.com>
2026-04-10 15:47:45 +0800

    修复资源文件未引入的问题

--- flutter/lib/mobile/pages/guide_page.dart
+++ flutter/lib/mobile/pages/guide_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../common.dart';
+import '../../models/platform_model.dart';
 import 'home_page.dart';
 
 class GuidePage extends StatefulWidget {
